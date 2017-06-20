b97ef33e1b5d332e563bdc17555577223a2bf2d5
Tong <tongwang01@gmail.com>
2017-06-20 10:39:37 -0700

    new run outputs with variation

--- code/lstm_cvae/generate_text.py
+++ code/lstm_cvae/generate_text.py
@@ -17,12 +17,12 @@ from lstm_cvae_model import ModelConfig
 
 def get_sample_config():
     sample_config = {
-        "model_dir": "/home/tongwang01/deepjoke/code/model_checkpoints/lstm_cvae/20170618_072219",
-        "starter_sentences": ["a man", "a girl", "a sexy", "what", "why", "i have a dream", "once upon a time", "trump"],
-        "temperatures": [None, 0.2, 0.5, 1.0, 1.5],
-        "scores": [0, 1, 5, 10, 20],
-        "num": 5
-    }
+        "model_dir": "/Users/tongwang/Playground/deepjoke/code/model_checkpoints/lstm_cvae/20170618_072219",
+        "starter_sentences": ["a sexy", "what", "why", "what's the best thing about living in switzerland", "once upon a time", "trump"],
+        "temperatures": [None, 0.2, 0.5, 1.0],
+        "scores": [1, 5, 10, 20],
+        "variations": [1., 2., 5.]
+        }
     return sample_config
 
 def softmax(x):
@@ -53,7 +53,7 @@ def tokens_to_words(tokens, tokenizer, eos=""):
     return text
 
 def generate_text(target_score, generator, model_config, tokenizer,
-                  starter_sentence="", temperature=None, eos=""):
+                  starter_sentence="", temperature=None, variation=1., eos=""):
     """Function to generate paragraphs given a target score, a random latent vector,
     and (optionally) a starter sentence.
     
@@ -67,7 +67,7 @@ def generate_text(target_score, generator, model_config, tokenizer,
         -model_config.batch_size many pieces of text
     """
     # Prepare inputs
-    z = np.random.normal(size=(model_config.batch_size, model_config.latent_size))
+    z = np.random.normal(scale=variation, size=(model_config.batch_size, model_config.latent_size))
     scores = np.repeat(target_score, model_config.batch_size)
     cur_sentence = [starter_sentence]
     cur_sequence = tokenizer.texts_to_sequences(cur_sentence)
@@ -78,21 +78,31 @@ def generate_text(target_score, generator, model_config, tokenizer,
     reverse_word_index = {index: word for word, index in tokenizer.word_index.iteritems()}
 
     # Iteratively predict the next word
+    true_lens = np.sum(cur_sequence > 0, axis=1)
+    last_tokens = np.array([cur_sequence[i, true_lens[i]-1] for i in range(model_config.batch_size)])   
     while True:
-        true_len = len(cur_sequence[0][cur_sequence[0]>0])
-        if true_len == model_config.max_sequence_length:
+        true_lens = np.sum(cur_sequence > 0, axis=1)
+        if np.min(np.logical_or(
+            (true_lens == model_config.max_sequence_length),
+            (last_tokens == 0))) > 0:
             break
-        next_preds = generator.predict([cur_sequence, scores, z])[0, true_len-1, :] # predicted next word
-        next_token = sample(next_preds, temperature)
-        if next_token == 0:
-            break
-        cur_sequence[0][true_len] = next_token
-    pred_sequence = cur_sequence[0][cur_sequence[0]>0]
+        all_preds = generator.predict([cur_sequence, scores, z])
+        for i in range(model_config.batch_size):
+            if last_tokens[i] == 0:
+                continue
+            else:
+                next_preds = all_preds[i, true_lens[i]-1, :]
+                next_token = sample(next_preds, temperature)
+                cur_sequence[i, true_lens[i]] = next_token
+                last_tokens[i] = next_token
 
-    # Translate tokens to words
-    pred_text = tokens_to_words(pred_sequence, tokenizer=tokenizer, eos=eos)
+    pred_texts = []
+    for i in range(model_config.batch_size):
+        pred_sequence = cur_sequence[i][cur_sequence[i]>0]
+        pred_text = tokens_to_words(pred_sequence, tokenizer=tokenizer, eos=eos)
+        pred_texts.append(pred_text)
     
-    return pred_text
+    return pred_texts
 
 def run_all():
     """Function to run ops"""
@@ -109,32 +119,35 @@ def run_all():
     generator = load_model(generator_path)
     tokenizer = pickle.load(open(tokenizer_path, "r"))
     model_config = pickle.load(open(model_config_path, "r"))
-    generated_text_path = model_dir + "/generated_text_{:%Y%m%d_%H%M%S}.csv".format(datetime.now())
+    generated_text_path = model_dir + "/generated_text_{:%Y%m%d_%H%M%S}.txt".format(datetime.now())
 
     # Generate text for each config, write to a csv file
     with open(generated_text_path, 'wb') as csvfile:
-        fieldnames = ["starter_sentence", "temperature", "score", "iter", "text"]
+        fieldnames = ["starter_sentence", "score", "variation", "temperature", "iter", "text"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="|")
         writer.writeheader()
         cnt = 0
         print("Generating texts...\n")    
         for starter_sentence in sample_config["starter_sentences"]:
-            for temperature in sample_config["temperatures"]:
-                for score in sample_config["scores"]:
-                    for i in range(sample_config["num"]):
-                        text = generate_text(generator=generator, model_config=model_config, tokenizer=tokenizer,
-                            target_score=score, starter_sentence=starter_sentence, temperature=temperature)
-                        new_dict_row = {
-                        "starter_sentence": starter_sentence,
-                        "temperature": str(temperature),
-                        "score": score,
-                        "iter": i,
-                        "text": text}
-                        writer.writerow(new_dict_row)
-                        csvfile.flush()
-                        cnt += 1
-                        print("Generated {} pieces of text".format(cnt))
-                        print(new_dict_row)
+            for score in sample_config["scores"]:
+                for temperature in sample_config["temperatures"]:
+                    for variation in sample_config["variations"]:
+                        texts = generate_text(generator=generator, model_config=model_config, tokenizer=tokenizer,
+                            target_score=score, starter_sentence=starter_sentence, variation=variation, temperature=temperature)
+                        for i in range(model_config.batch_size):
+                            text = texts[i]
+                            new_dict_row = {
+                                "starter_sentence": starter_sentence,
+                                "score": score,
+                                "variation": variation,
+                                "temperature": str(temperature),
+                                "iter": i,
+                                "text": text}
+                            writer.writerow(new_dict_row)
+                            csvfile.flush()
+                            print(new_dict_row)
+                        cnt += model_config.batch_size
+                        print("Generated {} pieces of text".format(cnt))               
 
 if __name__ == "__main__":
     run_all()
